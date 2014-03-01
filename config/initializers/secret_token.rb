e66c1a3b260d38095962981d4895a628d90be93c
Juan Mercedes <j.mercedes@thedesignpill.com>
2014-03-01 18:39:18 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DataExchange::Application.config.secret_key_base = 'd210d24f096a8a44ececae95a438634c7618c9b124f9c09ab577a29ac2cfbe9c641677af9943bae81aff36399e2a0dd0995291af9b214cafea73cc9318123548'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DataExchange::Application.config.session_store :cookie_store, key: '_data-exchange_session'
